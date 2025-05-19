fb5494a6c35a4198db33809311c93e68e5b9bf64
gaarangoa <gustavo1@vt.edu>
2025-05-19 12:22:32 -0400

    added setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='metk',
+    version='1.0.1',
+    author='Gustavo Arango',
+    author_email='',
+    description='Mutation Enrichment Toolkit',
+    packages=find_packages(),
+    install_requires=[
+        # List your module's dependencies here
+    ],
+    entry_points={
+        'console_scripts': [],
+    },
+)
