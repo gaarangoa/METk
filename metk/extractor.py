384b523a383c8f5c2efaaa594bc414c562edd51f
gaarangoa <gustavo1@vt.edu>
2025-05-21 15:53:13 -0400

    small fix

--- metk/extractor.py
+++ metk/extractor.py
@@ -10,6 +10,12 @@ from .dbnspf import DbnSPF
 
 import logging
 
+
+logging.basicConfig(
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    level=logging.INFO
+)
 logger = logging.getLogger()
 
 snpeff_items = [
